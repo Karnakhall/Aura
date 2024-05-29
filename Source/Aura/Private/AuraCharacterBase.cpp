fbdf1fb7de183da729054e32d8810ba0b8220c8a
Karnakhall <emil_q21@wp.pl>
2024-05-29 13:12:59 +0200

    Revert "Character/AuraCharacterBase"
    This reverts commit f913528ed164a9b1bbc0699089ff38f3c56b03f8.

--- Source/Aura/Private/AuraCharacterBase.cpp
+++ Source/Aura/Private/AuraCharacterBase.cpp
@@ -1,13 +1,13 @@
 // Copyright Karnakhall
 
 
-#include "Character/AuraCharacterBase.h"
+#include "AuraCharacterBase.h"
 
 // Sets default values
 AAuraCharacterBase::AAuraCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
 }
 
@@ -18,4 +18,17 @@ void AAuraCharacterBase::BeginPlay()
 	
 }
 
+// Called every frame
+void AAuraCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAuraCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
 
