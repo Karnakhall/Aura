fcaf8f75f4963cb5239b9cc923b0664ff6c66443
Karnakhall <emil_q21@wp.pl>
2024-05-29 14:19:50 +0200

    AuraCharacter

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright Karnakhall
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Copyright Karnakhall
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
