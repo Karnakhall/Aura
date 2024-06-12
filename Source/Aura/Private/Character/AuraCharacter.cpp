75066a83f07eccdd16a81081315f395a501a9b93
Karnakhall <emil_q21@wp.pl>
2024-06-12 13:54:58 +0200

    Animation and Aura character settings

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,17 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;	// Character moves in the direction of input...
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);	// ...at this rotation rate
+	GetCharacterMovement()->bConstrainToPlane = true;	// Constrain movement to the XY plane
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;	// Snap to the plane at the start
+
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
