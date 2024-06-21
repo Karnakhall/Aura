db80b495a0e1d931a9fb54e671c99a005b6587a1
Karnakhall <emil_q21@wp.pl>
2024-06-21 20:24:27 +0200

    Struct FWidgetController

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -3,3 +3,10 @@
 
 #include "UI/WidgetController/AuraWidgetController.h"
 
+void UAuraWidgetController::SetWidgetControllerParams(const FWidgetControllerParams& WCParams)
+{
+	PlayerController = WCParams.PlayerController;	// Set the PlayerController to the PlayerController from the WCParams
+	PlayerState = WCParams.PlayerState;	// Set the PlayerState to the PlayerState from the WCParams
+	AbilitySystemComponent = WCParams.AbilitySystemComponent;	// Set the AbilitySystemComponent to the AbilitySystemComponent from the WCParams
+	AttributeSet = WCParams.AttributeSet;	// Set the AttributeSet to the AttributeSet from the WCParams
+}
