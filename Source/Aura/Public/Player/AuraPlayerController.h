3cf67ba83cadd7bd9bc5d4e5f52cec09b8d14756
Karnakhall <emil_q21@wp.pl>
2024-10-09 11:56:02 +0200

    Callbacks for Ability Input

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -5,12 +5,16 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "EnhancedPlayerInput.h"
+#include "GameplayTagContainer.h"
 #include "AuraPlayerController.generated.h"
+
 
 class UInputMappingContext;
 class UInputAction;	
 struct FInputActionValue;	// Forward declaration
 class IEnemyInterface;	// Forward declaration
+class UAuraInputConfig;
+
 /**
  * 
  */
@@ -38,4 +42,12 @@ private:
 	
 	TScriptInterface<IEnemyInterface> LastActor;
 	TScriptInterface<IEnemyInterface> ThisActor;
+
+	void AbilityInputTagPressed(FGameplayTag InputTag);
+	void AbilityInputTagReleased(FGameplayTag InputTag);
+	void AbilityInputTagHeld(FGameplayTag InputTag);
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UAuraInputConfig> InputConfig;
 };
