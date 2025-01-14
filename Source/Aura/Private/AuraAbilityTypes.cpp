b12395d5f0716b13e9e5454f7835a054f425d54c
Karnakhall <emil_q21@wp.pl>
2025-01-14 10:38:23 +0100

    Added AuraAbilityTypes

---
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -0,0 +1,17 @@
+
+
+
+#include "AuraAbilityTypes.h"
+
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	return true;
+}
+
+AuraAbilityTypes::AuraAbilityTypes()
+{
+}
+
+AuraAbilityTypes::~AuraAbilityTypes()
+{
+}
