7c25b2e00d28a02b0079302abec12d706d76bc58
Karnakhall <emil_q21@wp.pl>
2025-01-15 19:27:00 +0100

    AuraAbilitySystemGlobals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,5 @@
+// Copyright Karnakhall
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemGlobals.h
@@ -0,0 +1,17 @@
+// Copyright Karnakhall
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemGlobals.h"
+#include "AuraAbilitySystemGlobals.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemGlobals : public UAbilitySystemGlobals
+{
+	GENERATED_BODY()
+	
+};
