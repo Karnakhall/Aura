cfdd85979b3d2fd76c3898dc78ef1d9a55aa572d
Karnakhall <emil_q21@wp.pl>
2025-02-05 13:45:18 +0100

    Using EQS Queries in behavior tees

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -159,8 +159,12 @@ void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag)
 					Spline->AddSplinePoint(PointLoc, ESplineCoordinateSpace::World);
 					//DrawDebugSphere(GetWorld(), PointLoc, 8.f, 8, FColor::Green, false, 5.f);
 				}
-				CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num() - 1];	// Set the destination to the last point
-				bAutoRunning = true;
+				if (NavPath->PathPoints.Num() > 0)
+				{
+					CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num() - 1];	// Set the destination to the last point
+					bAutoRunning = true;
+				}
+				
 			}
 		}
 		FollowTime = 0.f;
