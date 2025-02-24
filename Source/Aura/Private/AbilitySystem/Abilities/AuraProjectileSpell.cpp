912d5701988ecdbadf7c904e17d41a087077ab83
Karnakhall <emil_q21@wp.pl>
2025-02-24 15:40:28 +0100

    Montage and socket Tags

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -27,7 +27,7 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 
 	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(
 		GetAvatarActorFromActorInfo(), 
-		FAuraGameplayTags::Get().Montage_Attack_Weapon);		// Get the socket location from the combat interface
+		FAuraGameplayTags::Get().CombatSocket_Weapon);		// Get the socket location from the combat interface
 	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();		// Get the rotation of the projectile
 	//Rotation.Pitch = 0.f;		// Set the pitch to 0.f beacouse wa want to shoot our projectile near the ground
 
