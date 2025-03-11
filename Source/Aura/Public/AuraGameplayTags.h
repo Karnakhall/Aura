193fe9f9ac5a341c23c31058e07af03f19478f47
Karnakhall <emil_q21@wp.pl>
2025-03-11 15:01:22 +0100

    Gameplay ability cooldown

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -56,6 +56,9 @@ public:
 
 	FGameplayTag Abilities_Fire_FireBolt;
 
+	FGameplayTag Cooldown_Fire_FireBolt;
+
+
 	FGameplayTag CombatSocket_Weapon;
 	FGameplayTag CombatSocket_RightHand;
 	FGameplayTag CombatSocket_LeftHand;
