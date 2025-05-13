b187b5ffc5bfc1ee0f99feeb6cdd5d5c65ff295b
Karnakhall <emil_q21@wp.pl>
2025-05-13 12:32:29 +0200

    Debuff Parameters

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -208,6 +208,26 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FString("Debuff for Lightning damage")
 	);
 
+	GameplayTags.Debuff_Chance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Chance"),
+		FString("Debuff Chance")
+	);
+
+	GameplayTags.Debuff_Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Damage"),
+		FString("Debuff Damage")
+	);
+
+	GameplayTags.Debuff_Duration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Duration"),
+		FString("Debuff Duration")
+	);
+
+	GameplayTags.Debuff_Frequency = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Frequency"),
+		FString("Debuff Frequency")
+	);
+
 	/*
 	* Meta Attributes
 	*/
@@ -216,6 +236,7 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FName("Attributes.Meta.IncomingXP"),
 		FString("Incoming XP Meta Attribute")
 	);
+
 
 	/*
 	* Map of Damage Types to Resistance
